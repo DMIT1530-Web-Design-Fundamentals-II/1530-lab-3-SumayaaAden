be82f09b536551a5a267bbf81ee02fffc7fb8833
SumayaaAden <146494071+SumayaaAden@users.noreply.github.com>
2024-07-24 23:51:59 -0600

    Lab03 Complete!!!

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+const menuToggle = document.querySelector(".menu-icon img");
+const sidebar = document.querySelector("nav.sidebar");
+const menuIcon = document.querySelector(".menu-icon");
 
+const toggleMenu = () => {
+  sidebar.classList.toggle("open");
+  menuIcon.classList.toggle("active");
+};
+
+const closeMenu = () => {
+  sidebar.classList.remove("open");
+  menuIcon.classList.remove("active");
+};
+
+menuToggle.addEventListener("click", toggleMenu);
